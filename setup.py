21ed3c877657ab4e7100c0a0a7c733c638b24abb
yashanksh <yashankpandit420@gmail.com>
2023-12-18 23:40:13 +0530

    requirements added and virtual env setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Yashank Sharma',
+    author_email= 'yashanksharma10@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
