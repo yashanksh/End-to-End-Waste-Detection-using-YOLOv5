d8857d3b77762eddb432fefba24383d00b268d23
yashanksh <yashankpandit420@gmail.com>
2024-01-20 10:49:04 +0530

    data validation done

--- wasteDetection/constant/training_pipeline/__init__.py
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -8,3 +8,15 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1T2eaQdLRKqtKl8tdY7QDHQtNYm_nHvRP/view?usp=sharing"
+
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
